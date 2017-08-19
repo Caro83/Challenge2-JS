76f2cb8513818bed2cf00ab746ad8e2b68a0dcc4
Unknown <nora3110@gmail.com>
2017-08-19 17:42:10 +0200

    Ajout de boutons + alertes

--- assets/js/pomodoro.js
+++ assets/js/pomodoro.js
@@ -1,17 +1,69 @@
 $(function() {
-	var second = 1500;
+	var second;
 	var timer;
 	var once = true;
 	
 	function secondsTimeSpanToMS(s) {
-	    var m = Math.floor(s/60); //Get remaining minutes
+	    var m = Math.floor(s/60); 
 	    s -= m*60;
-	    return /*h+":"+*/(m < 10 ? '0'+m : m)+":"+(s < 10 ? '0'+s : s); 
+	    return (m < 10 ? '0'+m : m)+":"+(s < 10 ? '0'+s : s); 
 	}
-
 	
 
+	$("#work").click(function() {
+		second = 1500;
+		if(once) {
+			timer = setInterval(function() {
+			second-=1;
+			$("#timer").html(secondsTimeSpanToMS(second));
+			if (second === 0) {
+				clearInterval(timer);
+				alert("Travail terminé ! C'est l'heure de la pause");
+			}
+			}, 1000);
+
+			once = false;
+		}	
+	});
+
+	$("#break-5").click(function() {
+		second = 300;
+
+		if(once) {
+			timer = setInterval(function() {
+			second-=1;
+			$("#timer").html(secondsTimeSpanToMS(second));
+			if (second === 0) {
+				clearInterval(timer);
+				alert("Pause terminée ! Hop, au travail !");
+			}
+			}, 1000);
+
+			once = false;
+		}	
+
+	});
+
+	$("#break-15").click(function() {
+		second = 900;
+
+		if(once) {
+			timer = setInterval(function() {
+			second-=1;
+			$("#timer").html(secondsTimeSpanToMS(second));
+			if (second === 0) {
+				clearInterval(timer);
+				alert("Pause terminée ! Hop, au travail !");
+			}
+			}, 1000);
+
+			once = false;
+		}	
+
+	});
+
 	$("#start").click(function() {
+		second = second;
 		if(once) {
 			timer = setInterval(function() {
 			second-=1;
@@ -25,8 +77,16 @@ $(function() {
 		}	
 	});
 
+	$("#stop").click(function() {
+		clearInterval(timer);
+		$("#timer").html(secondsTimeSpanToMS(second));
+
+		once = true;
+
+	});	
+
 	$("#reset").click(function() {
-		second = 1500;
+		second = 0;
 
 		$("#timer").html(secondsTimeSpanToMS(0));
 
@@ -37,12 +97,5 @@ $(function() {
 		once = true;
 	});
 
-	$("#pause").click(function() {
-		clearInterval(timer);
-		$("#timer").html(secondsTimeSpanToMS(second));
-
-		once = true;
-
-	});	
 
 });
