6a2a8e7258231856ba7e6546095b79a2c4b02f83
Unknown <nora3110@gmail.com>
2017-08-19 15:24:32 +0200

    Amélioration du JS : Arrêt à 25min + conversion minutes/secondes

--- assets/js/pomodoro.js
+++ assets/js/pomodoro.js
@@ -1,13 +1,24 @@
 $(function() {
 	var second = 1500;
-	var time;
+	var timer;
 	var once = true;
 	
+	function secondsTimeSpanToMS(s) {
+	    var m = Math.floor(s/60); //Get remaining minutes
+	    s -= m*60;
+	    return /*h+":"+*/(m < 10 ? '0'+m : m)+":"+(s < 10 ? '0'+s : s); 
+	}
+
+	
+
 	$("#start").click(function() {
 		if(once) {
-			time = setInterval(function() {
+			timer = setInterval(function() {
 			second-=1;
-			$("#compteur").html(second);
+			$("#timer").html(secondsTimeSpanToMS(second));
+			if (second === 0) {
+				clearInterval(timer);
+			}
 			}, 1000);
 
 			once = false;
@@ -17,18 +28,18 @@ $(function() {
 	$("#reset").click(function() {
 		second = 1500;
 
-		$("#compteur").html("0");
+		$("#timer").html(secondsTimeSpanToMS(0));
 
 		if (once === false) {
-			clearInterval(time);
+			clearInterval(timer);
 		}
 
 		once = true;
 	});
 
 	$("#pause").click(function() {
-		clearInterval(time);
-		$("#compteur").html(second);
+		clearInterval(timer);
+		$("#timer").html(secondsTimeSpanToMS(second));
 
 		once = true;
 
